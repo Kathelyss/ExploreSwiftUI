fb84bc052f895a394d56b663e9cc248ff2070cd5
keithryd <rubicante@icloud.com>
2022-10-01 23:14:01 +0300

    Add project

---
+++ Shared/ExploreSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreSwiftUIApp.swift
+//  Shared
+//
+//  Created by Katik on 01.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExploreSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
