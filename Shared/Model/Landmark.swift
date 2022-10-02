947da898da672fc8354f121c882f4b4264990a9c
keithryd <rubicante@icloud.com>
2022-10-02 23:29:02 +0300

    Section 5 result: Identifiable protocol

--- Shared/Model/Landmark.swift
+++ Shared/Model/Landmark.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     let id: Int
     let name: String
     let park: String
