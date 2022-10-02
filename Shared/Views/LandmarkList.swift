7d96649cf63228c7cd397e3c4111ccd6290bdaa9
keithryd <rubicante@icloud.com>
2022-10-02 23:22:14 +0300

    Section 4 result: add List

---
+++ Shared/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  ExploreSwiftUI (iOS)
+//
+//  Created by Kathelyss on 02.10.2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
