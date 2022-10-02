75db2052292bbe7171187c7a72558b80504db206
keithryd <rubicante@icloud.com>
2022-10-03 00:12:50 +0300

    Section 8 result: add preview devices list

--- Shared/Views/LandmarkList.swift
+++ Shared/Views/LandmarkList.swift
@@ -24,6 +24,13 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (2nd generation)",
+                 "iPhone XS Max",
+                 "iPad (9th generation)"],
+                id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
