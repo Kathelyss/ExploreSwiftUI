e00ff9bcb051c59c45f3a52debe82a8a26c17456
keithryd <rubicante@icloud.com>
2022-10-03 00:00:38 +0300

    Section 7 result: Pass data into child views

--- Shared/Views/MapView.swift
+++ Shared/Views/MapView.swift
@@ -9,17 +9,27 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @State private var region = MKCoordinateRegion()
+
+    var coordinate: CLLocationCoordinate2D
     
     var body: some View {
         Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
+            }
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(center: coordinate,
+                                    span: .init(latitudeDelta: 0.2,
+                                                longitudeDelta: 0.2))
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: .init(latitude: 34.011_286,
+                                  longitude: -116.166_868))
     }
 }
