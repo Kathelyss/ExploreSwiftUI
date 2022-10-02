835aeb1be144406bee475dbd940fe650ca55b7fe
keithryd <rubicante@icloud.com>
2022-10-02 22:28:30 +0300

    Section 1 result: add model, images and files grouping

---
+++ Shared/Model/Landmark.swift
@@ -0,0 +1,35 @@
+//
+//  Landmark.swift
+//  ExploreSwiftUI (iOS)
+//
+//  Created by Kathelyss on 02.10.2022.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    let id: Int
+    let name: String
+    let park: String
+    let state: String
+    let category: String
+    let description: String
+    
+    private var imageName: String
+    var image: Image {
+        .init(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        .init(latitude: coordinates.lattitude,
+              longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        let lattitude: Double
+        let longitude: Double
+    }
+}
---
+++ Shared/Model/ModelData.swift
@@ -0,0 +1,31 @@
+//
+//  ModelData.swift
+//  ExploreSwiftUI (iOS)
+//
+//  Created by Kathelyss on 02.10.2022.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
